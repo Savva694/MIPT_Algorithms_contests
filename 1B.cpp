9631e8ddc7000cadaf277ce88830c3dcd538822c
Savva Chuev <74358817+Savva694@users.noreply.github.com>
2023-10-26 00:49:07 +0300

    Update 1B.cpp

--- 1B.cpp
+++ 1B.cpp
@@ -7,17 +7,15 @@ int main() {
   const int kNumsAfterComma = 6;
   int length;
   std::cin >> length;
-  std::vector<double> volume;
-  std::vector<double> log_vol;
-  volume.resize(length);
-  log_vol.resize(length + 1);
+  std::vector<double> volume(length);
+  std::vector<double> log_volume(length + 1);
   for (int i = 0; i < length; ++i) {
     std::cin >> volume[i];
     volume[i] = std::log2(volume[i]);
   }
-  log_vol[0] = 0;
+  log_volume[0] = 0;
   for (int i = 1; i <= length; ++i) {
-    log_vol[i] = (log_vol[i - 1] + volume[i - 1]);
+    log_volume[i] = (log_volume[i - 1] + volume[i - 1]);
   }
   int questions;
   std::cin >> questions;
@@ -26,7 +24,7 @@ int main() {
   for (int i = 0; i < questions; ++i) {
     std::cin >> left >> right;
     std::cout << std::fixed << std::setprecision(kNumsAfterComma)
-              << std::pow(2, (log_vol[right + 1] - log_vol[left]) /
+              << std::pow(2, (log_volume[right + 1] - log_volume[left]) /
                                      (right - left + 1))
               << "\n";
   }
