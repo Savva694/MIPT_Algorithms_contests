edb1e0c0b781abdcd1b6e1f436c9b2e3366c5713
Savva Chuev <74358817+Savva694@users.noreply.github.com>
2023-10-26 20:47:08 +0300

    Update 1G.cpp

--- 1G.cpp
+++ 1G.cpp
@@ -2,50 +2,33 @@
 #include <string>
 #include <vector>
 
-int main() {
-  std::ios_base::sync_with_stdio(false);
-  std::cin.tie(NULL);
-  int length;
-  std::cin >> length;
-  std::vector<std::pair<std::string, std::string>> numbers;
-  numbers.resize(length);
-  int max_length;
+void LongLongLSD(std::vector<std::pair<std::string, std::string>>& numbers,
+                 int length, int max_length) {
   int now_length;
-  max_length = 0;
   const int kNumberSystem = 10;
   const int kNullInChar = 48;
   for (int i = 0; i < length; ++i) {
-    std::cin >> numbers[i].second;
-    numbers[i].first = numbers[i].second;
-    now_length = numbers[i].first.length();
-    if (now_length > max_length) {
-      max_length = now_length;
-    }
-  }
-  for (int i = 0; i < length; ++i) {
-    now_length = numbers[i].first.length();
+    now_length = static_cast<int>(numbers[i].first.length());
     for (int j = 0; j < max_length - now_length; ++j) {
       numbers[i].first = "0" + numbers[i].first;
     }
   }
-  std::vector<std::pair<std::string, std::string>> numbers_copy;
-  std::vector<int> count;
-  numbers_copy.resize(length);
-  count.resize(kNumberSystem);
+  std::vector<std::pair<std::string, std::string>> numbers_copy(length);
+  std::vector<int> count(kNumberSystem);
   int number;
   for (int i = max_length - 1; i >= 0; --i) {
     for (int j = 0; j < kNumberSystem; ++j) {
       count[j] = 0;
     }
     for (int j = 0; j < length; ++j) {
-      ++count[(int) numbers[j].first[i] - kNullInChar];
+      ++count[static_cast<int>(numbers[j].first[i] - kNullInChar)];
     }
     for (int j = 1; j < kNumberSystem; ++j) {
       count[j] += count[j - 1];
     }
 
     for (int j = length - 1; j >= 0; --j) {
-      number = (int) numbers[j].first[i] - kNullInChar;
+      number = static_cast<int>(numbers[j].first[i] - kNullInChar);
       numbers_copy[count[number] - 1] = numbers[j];
       --count[number];
     }
@@ -53,6 +36,23 @@ int main() {
       numbers[j] = numbers_copy[j];
     }
   }
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  int length;
+  std::cin >> length;
+  std::vector<std::pair<std::string, std::string>> numbers(length);
+  int max_length = 0;
+  int now_length;
+  for (int i = 0; i < length; ++i) {
+    std::cin >> numbers[i].second;
+    numbers[i].first = numbers[i].second;
+    now_length = static_cast<int>(numbers[i].first.length());
+    max_length = std::max(max_length, now_length);
+  }
+  LongLongLSD(numbers, length, max_length);
   for (int i = 0; i < length; ++i) {
     std::cout << numbers[i].second << "\n";
   }
