0dc0487458b3035d7a0775dd4ce8704484281e62
Savva Chuev <74358817+Savva694@users.noreply.github.com>
2023-10-26 14:20:52 +0300

    Update 1F.cpp
    https://contest.yandex.ru/contest/52563/run-report/94665260/

--- 1F.cpp
+++ 1F.cpp
@@ -1,12 +1,6 @@
 #include <iostream>
 #include <vector>
 
-const int kLocalMin = 10004321;
-const int kConditionNum1 = 123;
-const int kConditionNum2 = 45;
-const int kTen = 10;
-std::vector<int> vector_for_long_sort(kTen);
-
 int MedianOfFiveNumbers(int num1, int num2, int num3, int num4, int num5) {
   if (num1 > num2) {
     std::swap(num1, num2);
@@ -29,7 +23,8 @@ int MedianOfFiveNumbers(int num1, int num2, int num3, int num4, int num5) {
   return std::min(num3, num4);
 }
 
-int LongSort(int length, int position) {
+int LongSort(int length, int position, std::vector<int>& vector_for_long_sort) {
+  const int kLocalMin = 10004321;
   int local_min;
   int min_index;
   min_index = 0;
@@ -48,57 +43,61 @@ int LongSort(int length, int position) {
   return vector_for_long_sort[position];
 }
 
-int DetQuickSelect(std::vector<int>* numbers, int position, int left,
-                   int right) {
+int DetQuickSelect(std::vector<int>& numbers, int position, int left, int right,
+                   std::vector<int>& vector_for_long_sort) {
+  const int kLocalMin = 10004321;
+  const int kTen = 10;
   int length = right - left + 1;
   if (length <= kTen) {
     for (int i = left; i <= right; ++i) {
-      vector_for_long_sort[i - left] = (*numbers)[i];
+      vector_for_long_sort[i - left] = numbers[i];
     }
-    return LongSort(length, position - left);
+    return LongSort(length, position - left, vector_for_long_sort);
   }
-  int degree_five;
-  degree_five = length;
+  int degree_five = length;
   while (degree_five % 5 != 0) {
     ++degree_five;
   }
-  std::vector<int> five_nums_medians;
-  five_nums_medians.resize(degree_five / 5 + 5, kLocalMin);
+  std::vector<int> five_nums_medians(degree_five / 5 + 5, kLocalMin);
   for (int i = left; i < left + degree_five; i += 5) {
     five_nums_medians[(i - left) / 5] =
-        MedianOfFiveNumbers((*numbers)[i], (*numbers)[i + 1], (*numbers)[i + 2],
-                            (*numbers)[i + 3], (*numbers)[i + 4]);
+        MedianOfFiveNumbers(numbers[i], numbers[i + 1], numbers[i + 2],
+                            numbers[i + 3], numbers[i + 4]);
   }
-  int part_elem;
-  part_elem = DetQuickSelect(&five_nums_medians, degree_five / kTen, 0,
-                             degree_five / 5 - 1);
+  int part_elem = DetQuickSelect(five_nums_medians, degree_five / kTen, 0,
+                                 degree_five / 5 - 1, vector_for_long_sort);
   int indicator = left;
-  int left_pivot;
-  int right_pivot;
   for (int i = left; i <= right; ++i) {
-    if ((*numbers)[i] < part_elem) {
-      std::swap((*numbers)[i], (*numbers)[indicator]);
+    if (numbers[i] < part_elem) {
+      std::swap(numbers[i], numbers[indicator]);
       ++indicator;
     }
   }
-  left_pivot = indicator;
+  int left_pivot = indicator;
   for (int i = left_pivot; i <= right; ++i) {
-    if ((*numbers)[i] == part_elem) {
-      std::swap((*numbers)[i], (*numbers)[indicator]);
+    if (numbers[i] == part_elem) {
+      std::swap(numbers[i], numbers[indicator]);
       ++indicator;
     }
   }
-  right_pivot = indicator - 1;
+  int right_pivot = indicator - 1;
   if (left_pivot > position) {
-    return DetQuickSelect(numbers, position, left, left_pivot - 1);
+    return DetQuickSelect(numbers, position, left, left_pivot - 1,
+                          vector_for_long_sort);
   }
   if (left_pivot <= position && position <= right_pivot) {
     return part_elem;
   }
-  return DetQuickSelect(numbers, position, right_pivot + 1, right);
+  return DetQuickSelect(numbers, position, right_pivot + 1, right,
+                        vector_for_long_sort);
 }
 
 int main() {
+  const int kLocalMin = 10004321;
+  const int kConditionNum1 = 123;
+  const int kConditionNum2 = 45;
+  const int kTen = 10;
+  std::vector<int> vector_for_long_sort(kTen);
   int length;
   int position;
   std::cin >> length;
@@ -109,8 +108,9 @@ int main() {
   for (int i = 2; i < length; ++i) {
     numbers[i] =
         (numbers[i - 1] * kConditionNum1 + numbers[i - 2] * kConditionNum2) %
-        (kLocalMin);
+        kLocalMin;
   }
-  std::cout << DetQuickSelect(&numbers, position - 1, 0, length - 1);
+  std::cout << DetQuickSelect(numbers, position - 1, 0, length - 1,
+                              vector_for_long_sort);
   return 0;
 }
