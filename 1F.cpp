2d9fcf86e8cb3f8ae8f5601c2f205d1c922f9498
Savva Chuev <74358817+Savva694@users.noreply.github.com>
2023-10-26 21:03:02 +0300

    Update 1F.cpp

--- 1F.cpp
+++ 1F.cpp
@@ -24,12 +24,11 @@ int MedianOfFiveNumbers(int num1, int num2, int num3, int num4, int num5) {
 }
 
 int LongSort(int length, int position, std::vector<int>& vector_for_long_sort) {
-  const int kLocalMin = 10004321;
+  const int kLocalMax = 10004321;
   int local_min;
-  int min_index;
-  min_index = 0;
+  int min_index = 0;
   for (int i = 0; i < length; ++i) {
-    local_min = kLocalMin + 1;
+    local_min = kLocalMax + 1;
     for (int j = i; j < length; ++j) {
       if (vector_for_long_sort[j] < local_min) {
         local_min = vector_for_long_sort[j];
@@ -43,12 +42,13 @@ int LongSort(int length, int position, std::vector<int>& vector_for_long_sort) {
   return vector_for_long_sort[position];
 }
 
-int DetQuickSelect(std::vector<int>& numbers, int position, int left, int right,
-                   std::vector<int>& vector_for_long_sort) {
-  const int kLocalMin = 10004321;
-  const int kTen = 10;
+int DetQuickSelect(std::vector<int>& numbers, int position, int left,
+                   int right) {
+  const int kLocalMax = 10004321;
+  const int kBaseLength = 10;
+  std::vector<int> vector_for_long_sort(kBaseLength);
   int length = right - left + 1;
-  if (length <= kTen) {
+  if (length <= kBaseLength) {
     for (int i = left; i <= right; ++i) {
       vector_for_long_sort[i - left] = numbers[i];
     }
@@ -58,19 +58,19 @@ int DetQuickSelect(std::vector<int>& numbers, int position, int left, int right,
   while (degree_five % 5 != 0) {
     ++degree_five;
   }
-  std::vector<int> five_nums_medians(degree_five / 5 + 5, kLocalMin);
+  std::vector<int> five_nums_medians(degree_five / 5 + 5, kLocalMax);
   for (int i = left; i < left + degree_five; i += 5) {
     five_nums_medians[(i - left) / 5] =
         MedianOfFiveNumbers(numbers[i], numbers[i + 1], numbers[i + 2],
                             numbers[i + 3], numbers[i + 4]);
   }
-  int part_elem = DetQuickSelect(five_nums_medians, degree_five / kTen, 0,
-                                 degree_five / 5 - 1, vector_for_long_sort);
+  const int kNewPosition = 10;
+  int part_elem = DetQuickSelect(five_nums_medians, degree_five / kNewPosition,
+                                 0, degree_five / 5 - 1);
   int indicator = left;
   for (int i = left; i <= right; ++i) {
     if (numbers[i] < part_elem) {
-      std::swap(numbers[i], numbers[indicator]);
-      ++indicator;
+      std::swap(numbers[i], numbers[indicator++]);
     }
   }
   int left_pivot = indicator;
@@ -82,35 +82,29 @@ int DetQuickSelect(std::vector<int>& numbers, int position, int left, int right,
   }
   int right_pivot = indicator - 1;
   if (left_pivot > position) {
-    return DetQuickSelect(numbers, position, left, left_pivot - 1,
-                          vector_for_long_sort);
+    return DetQuickSelect(numbers, position, left, left_pivot - 1);
   }
   if (left_pivot <= position && position <= right_pivot) {
     return part_elem;
   }
-  return DetQuickSelect(numbers, position, right_pivot + 1, right,
-                        vector_for_long_sort);
+  return DetQuickSelect(numbers, position, right_pivot + 1, right);
 }
 
 int main() {
-  const int kLocalMin = 10004321;
+  const int kLocalMax = 10004321;
   const int kConditionNum1 = 123;
   const int kConditionNum2 = 45;
-  const int kTen = 10;
-  std::vector<int> vector_for_long_sort(kTen);
   int length;
   int position;
   std::cin >> length;
   std::cin >> position;
-  std::vector<int> numbers;
-  numbers.resize(length + 5, kLocalMin);
+  std::vector<int> numbers(length + 5);
   std::cin >> numbers[0] >> numbers[1];
   for (int i = 2; i < length; ++i) {
     numbers[i] =
         (numbers[i - 1] * kConditionNum1 + numbers[i - 2] * kConditionNum2) %
-        kLocalMin;
+        kLocalMax;
   }
-  std::cout << DetQuickSelect(numbers, position - 1, 0, length - 1,
-                              vector_for_long_sort);
+  std::cout << DetQuickSelect(numbers, position - 1, 0, length - 1);
   return 0;
 }
