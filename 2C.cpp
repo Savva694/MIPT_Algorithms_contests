55c6102f69d268418255335caf88a77e1f9a8a9f
Savva Chuev <74358817+Savva694@users.noreply.github.com>
2023-11-14 12:13:47 +0300

    Update 2C.cpp

--- 2C.cpp
+++ 2C.cpp
@@ -2,33 +2,51 @@
 #include <iostream>
 #include <string>
 
-void MakeDequeCorrect(std::deque<int>& left, std::deque<int>& right) {
-  if (left.size() > right.size()) {
-    right.push_back(left.front());
-    left.pop_front();
+struct GoblinDeque {
+  std::deque<int> left;
+  std::deque<int> right;
+
+  void MakeDequeCorrect() {
+    if (left.size() > right.size()) {
+      right.push_back(left.front());
+      left.pop_front();
+    }
   }
-}
+
+  void AddNewOrdinaryGoblin(int goblin) {
+    left.push_back(goblin);
+    this->MakeDequeCorrect();
+  }
+
+  void AddNewPrivilegedGoblin(int goblin) {
+    left.push_front(goblin);
+    this->MakeDequeCorrect();
+  }
+
+  void DeleteGoblin() {
+    std::cout << right.front() << std::endl;
+    right.pop_front();
+    this->MakeDequeCorrect();
+  }
+};
 
 int main() {
   int requests;
   std::cin >> requests;
-  std::deque<int> left;
-  std::deque<int> right;
+  GoblinDeque goblin_queue;
   std::string sign;
   int goblin;
   for (int request = 0; request < requests; ++request) {
     std::cin >> sign;
     if (sign == "-") {
-      std::cout << right.front() << std::endl;
-      right.pop_front();
+      goblin_queue.DeleteGoblin();
     } else if (sign == "+") {
       std::cin >> goblin;
-      left.push_back(goblin);
+      goblin_queue.AddNewOrdinaryGoblin(goblin);
     } else {
       std::cin >> goblin;
-      left.push_front(goblin);
+      goblin_queue.AddNewPrivilegedGoblin(goblin);
     }
-    MakeDequeCorrect(left, right);
   }
   return 0;
 }
