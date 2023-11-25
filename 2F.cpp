592689785bad2039597fed1a23e9911d74dfc5d9
Savva Chuev <74358817+Savva694@users.noreply.github.com>
2023-11-25 16:09:07 +0300

    Update 2F.cpp

--- 2F.cpp
+++ 2F.cpp
@@ -2,19 +2,23 @@
 #include <string>
 #include <vector>
 
-class Binaryheap {
+class BinaryHeap {
  private:
   std::vector<std::pair<long long, int>> heap_;
   std::vector<int> operations_;
   int length_ = 0;
 
+  void SwapNodes(int& index_1, int index_2) {
+    std::swap(operations_[heap_[index_1].second],
+              operations_[heap_[index_2].second]);
+    std::swap(heap_[index_1], heap_[index_2]);
+    index_1 = index_2;
+  }
+
  public:
   void SiftUp(int index) {
     while (index > 0 && heap_[index].first < heap_[(index - 1) / 2].first) {
-      std::swap(operations_[heap_[index].second],
-                operations_[heap_[(index - 1) / 2].second]);
-      std::swap(heap_[index], heap_[(index - 1) / 2]);
-      index = (index - 1) / 2;
+      SwapNodes(index, (index - 1) / 2);
     }
   }
 
@@ -22,10 +26,7 @@ class Binaryheap {
     while (2 * index + 1 <= length_ - 1) {
       if (2 * index + 1 == length_ - 1) {
         if (heap_[index].first > heap_[2 * index + 1].first) {
-          std::swap(operations_[heap_[index].second],
-                    operations_[heap_[2 * index + 1].second]);
-          std::swap(heap_[index], heap_[2 * index + 1]);
-          index = 2 * index + 1;
+          SwapNodes(index, 2 * index + 1);
         } else {
           break;
         }
@@ -33,15 +34,9 @@ class Binaryheap {
         if (heap_[index].first > heap_[2 * index + 1].first ||
             heap_[index].first > heap_[2 * index + 2].first) {
           if (heap_[2 * index + 1].first > heap_[2 * index + 2].first) {
-            std::swap(operations_[heap_[index].second],
-                      operations_[heap_[2 * index + 2].second]);
-            std::swap(heap_[index], heap_[2 * index + 2]);
-            index = 2 * index + 2;
+            SwapNodes(index, 2 * index + 2);
           } else {
-            std::swap(operations_[heap_[index].second],
-                      operations_[heap_[2 * index + 1].second]);
-            std::swap(heap_[index], heap_[2 * index + 1]);
-            index = 2 * index + 1;
+            SwapNodes(index, 2 * index + 1);
           }
         } else {
           break;
@@ -54,7 +49,7 @@ class Binaryheap {
     operations_.push_back(length_++);
     heap_.push_back(
         std::make_pair(number, static_cast<int>(operations_.size()) - 1));
-    this->SiftUp(length_ - 1);
+    SiftUp(length_ - 1);
   }
 
   long long GetMin() {
@@ -68,13 +63,13 @@ class Binaryheap {
               operations_[heap_[length_ - 1].second]);
     std::swap(heap_[0], heap_[--length_]);
     heap_.pop_back();
-    this->SiftDown(0);
+    SiftDown(0);
   }
 
   void DecreaseKey(int operation_num, long long number) {
     operations_.push_back(-1);
     heap_[operations_[operation_num - 1]].first -= number;
-    this->SiftUp(operations_[operation_num - 1]);
+    SiftUp(operations_[operation_num - 1]);
   }
 };
 
@@ -83,7 +78,7 @@ int main() {
   std::cin.tie(nullptr);
   int questions;
   std::cin >> questions;
-  Binaryheap binary_heap;
+  BinaryHeap binary_heap;
   std::string command;
   long long number;
   int operation_num;
