0bde8d17004d29876d98fb6461420c41dfb51d46
Savva Chuev <74358817+Savva694@users.noreply.github.com>
2024-04-08 17:02:22 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,38 +1,46 @@
 #include <algorithm>
 #include <iostream>
-#include <set>
 #include <stack>
 #include <vector>
 
-size_t my_time = 0;
-
 struct Node {
-  size_t vert;
+  size_t vertex;
   int color;
-  size_t tin;
-  size_t tout;
-  size_t ret;
+  size_t time_in;
+  size_t time_out;
+  size_t highest_reachable_vertex;
   size_t parent;
 
-  Node() : vert(0), color(0), tin(0), tout(0), ret(0), parent(0) {}
+  Node()
+      : vertex(0),
+        color(0),
+        time_in(0),
+        time_out(0),
+        highest_reachable_vertex(0),
+        parent(0) {}
 
-  Node(size_t vert)
-      : vert(vert), color(0), tin(0), tout(0), ret(0), parent(0) {}
+  Node(size_t vertex)
+      : vertex(vertex),
+        color(0),
+        time_in(0),
+        time_out(0),
+        highest_reachable_vertex(0),
+        parent(0) {}
 
   Node(const Node& other)
-      : vert(other.vert),
+      : vertex(other.vertex),
         color(other.color),
-        tin(other.tin),
-        tout(other.tout),
-        ret(other.ret),
+        time_in(other.time_in),
+        time_out(other.time_out),
+        highest_reachable_vertex(other.highest_reachable_vertex),
         parent(other.parent) {}
 
   Node& operator=(const Node& other) {
-    vert = other.vert;
+    vertex = other.vertex;
     color = other.color;
-    tin = other.tin;
-    tout = other.tout;
-    ret = other.ret;
+    time_in = other.time_in;
+    time_out = other.time_out;
+    highest_reachable_vertex = other.highest_reachable_vertex;
     parent = other.parent;
     return *this;
   }
@@ -54,58 +62,89 @@ struct Edge {
         edge_id(other.edge_id) {}
 };
 
+namespace DfsVariables {
+size_t my_time = 0;
 std::vector<Node> vertices;
 std::vector<std::vector<Edge>> edges;
 std::vector<size_t> bridges;
+}  // namespace DfsVariables
 
 void Dfs(size_t vertex) {
-  ++my_time;
-  vertices[vertex].color = 1;
-  vertices[vertex].tin = my_time;
-  for (Edge neighbour : edges[vertex]) {
-    if (vertices[neighbour.vertex_2].color == 0) {
-      vertices[neighbour.vertex_2].parent = vertex;
-      ++my_time;
+  ++DfsVariables::my_time;
+  DfsVariables::vertices[vertex].color = 1;
+  DfsVariables::vertices[vertex].time_in = DfsVariables::my_time;
+  for (Edge neighbour : DfsVariables::edges[vertex]) {
+    if (DfsVariables::vertices[neighbour.vertex_2].color == 0) {
+      DfsVariables::vertices[neighbour.vertex_2].parent = vertex;
       Dfs(neighbour.vertex_2);
-      ++my_time;
+      ++DfsVariables::my_time;
     }
   }
-  vertices[vertex].color = 2;
-  vertices[vertex].tout = my_time;
+  DfsVariables::vertices[vertex].color = 2;
+  DfsVariables::vertices[vertex].time_out = DfsVariables::my_time;
 }
 
 void RetCounting(size_t vertex) {
-  vertices[vertex].color = 1;
-  vertices[vertex].ret = vertices[vertex].tin;
-  for (size_t index = 0; index < edges[vertex].size(); ++index) {
-    Edge neighbour = edges[vertex][index];
-    if (vertices[vertex].tin >= vertices[neighbour.vertex_2].tin &&
-        vertices[vertex].tout <= vertices[neighbour.vertex_2].tout &&
-        vertices[vertex].parent != neighbour.vertex_2) {
-      vertices[vertex].ret =
-          std::min(vertices[vertex].ret, vertices[neighbour.vertex_2].tin);
+  DfsVariables::vertices[vertex].color = 1;
+  DfsVariables::vertices[vertex].highest_reachable_vertex =
+      DfsVariables::vertices[vertex].time_in;
+  for (size_t index = 0; index < DfsVariables::edges[vertex].size(); ++index) {
+    Edge neighbour = DfsVariables::edges[vertex][index];
+    if (DfsVariables::vertices[vertex].time_in >=
+            DfsVariables::vertices[neighbour.vertex_2].time_in &&
+        DfsVariables::vertices[vertex].time_out <=
+            DfsVariables::vertices[neighbour.vertex_2].time_out &&
+        DfsVariables::vertices[vertex].parent != neighbour.vertex_2) {
+      DfsVariables::vertices[vertex].highest_reachable_vertex =
+          std::min(DfsVariables::vertices[vertex].highest_reachable_vertex,
+                   DfsVariables::vertices[neighbour.vertex_2].time_in);
     }
-    if (vertices[neighbour.vertex_2].color == 0) {
-      vertices[neighbour.vertex_2].color = 1;
+    if (DfsVariables::vertices[neighbour.vertex_2].color == 0) {
+      DfsVariables::vertices[neighbour.vertex_2].color = 1;
       RetCounting(neighbour.vertex_2);
-      if (vertices[vertex].parent != neighbour.vertex_2) {
-        vertices[vertex].ret =
-            std::min(vertices[vertex].ret, vertices[neighbour.vertex_2].ret);
+      if (DfsVariables::vertices[vertex].parent != neighbour.vertex_2) {
+        DfsVariables::vertices[vertex].highest_reachable_vertex =
+            std::min(DfsVariables::vertices[vertex].highest_reachable_vertex,
+                     DfsVariables::vertices[neighbour.vertex_2]
+                         .highest_reachable_vertex);
       }
-      if (vertices[vertex].tin < vertices[neighbour.vertex_2].ret) {
+      if (DfsVariables::vertices[vertex].time_in <
+          DfsVariables::vertices[neighbour.vertex_2].highest_reachable_vertex) {
         size_t vertex_count = 0;
-        for (Edge edge : edges[vertex]) {
+        for (Edge edge : DfsVariables::edges[vertex]) {
           if (edge.vertex_2 == neighbour.vertex_2) {
             ++vertex_count;
           }
         }
         if (vertex_count == 1) {
-          bridges.push_back(edges[vertex][index].edge_id);
+          DfsVariables::bridges.push_back(
+              DfsVariables::edges[vertex][index].edge_id);
         }
       }
     }
   }
-  vertices[vertex].color = 2;
+  DfsVariables::vertices[vertex].color = 2;
+}
+
+void FindBridges() {
+  size_t vertex_count = DfsVariables::vertices.size();
+  for (size_t i = 0; i < vertex_count; ++i) {
+    if (DfsVariables::vertices[i].color == 0) {
+      Dfs(i);
+    }
+  }
+
+  for (size_t i = 0; i < vertex_count; ++i) {
+    DfsVariables::vertices[i].color = 0;
+  }
+
+  for (size_t i = 0; i < vertex_count; ++i) {
+    if (DfsVariables::vertices[i].color == 0) {
+      RetCounting(i);
+    }
+  }
+
+  sort(DfsVariables::bridges.begin(), DfsVariables::bridges.end());
 }
 
 int main() {
@@ -113,11 +152,11 @@ int main() {
   size_t edge_count;
   std::cin >> vertex_count;
   std::cin >> edge_count;
-  vertices.resize(vertex_count);
-  edges.resize(vertex_count);
+  DfsVariables::vertices.resize(vertex_count);
+  DfsVariables::edges.resize(vertex_count);
 
   for (size_t i = 0; i < vertex_count; ++i) {
-    vertices[i] = Node(i);
+    DfsVariables::vertices[i] = Node(i);
   }
 
   size_t vertex_1;
@@ -125,29 +164,16 @@ int main() {
   for (size_t i = 0; i < edge_count; ++i) {
     std::cin >> vertex_1;
     std::cin >> vertex_2;
-    edges[vertex_1 - 1].push_back(Edge(vertex_1 - 1, vertex_2 - 1, i));
-    edges[vertex_2 - 1].push_back(Edge(vertex_2 - 1, vertex_1 - 1, i));
+    DfsVariables::edges[vertex_1 - 1].emplace_back(vertex_1 - 1, vertex_2 - 1,
+                                                   i);
+    DfsVariables::edges[vertex_2 - 1].emplace_back(vertex_2 - 1, vertex_1 - 1,
+                                                   i);
   }
 
-  for (size_t i = 0; i < vertex_count; ++i) {
-    if (vertices[i].color == 0) {
-      Dfs(i);
-    }
-  }
+  FindBridges();
 
-  for (size_t i = 0; i < vertex_count; ++i) {
-    vertices[i].color = 0;
-  }
-
-  for (size_t i = 0; i < vertex_count; ++i) {
-    if (vertices[i].color == 0) {
-      RetCounting(i);
-    }
-  }
-
-  sort(bridges.begin(), bridges.end());
-  std::cout << bridges.size() << "\n";
-  for (size_t bridge : bridges) {
+  std::cout << DfsVariables::bridges.size() << "\n";
+  for (size_t bridge : DfsVariables::bridges) {
     std::cout << bridge + 1 << "\n";
   }
 }
